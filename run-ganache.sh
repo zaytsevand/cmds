082c068d4da22385d2322bd48222139707a7fedb
Andrey Zaytsev <zaytsevand@outlook.com>
2018-04-20 13:27:10 +0300

    ganache + pgadmin

---
+++ run-ganache.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d -p 8545:8545 trufflesuite/ganache-cli:latest -l 4000000
---
+++ run-pgadmin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~/pgadmin4
+source bin/activate
+python3 lib/python3.5/site-packages/pgadmin4/pgAdmin4.py
+
